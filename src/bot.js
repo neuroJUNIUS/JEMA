6fe86d0215fdd8bfd83bdf73999edf1e295ddc7b
ernestasgr <ernestasgr02@gmail.com>
2022-04-05 18:40:54 +0300

    Completed reaction getting function

--- src/bot.js
+++ src/bot.js
@@ -1,8 +1,12 @@
 require('dotenv').config();
 const { Client, Collection, Intents } = require('discord.js');
-const client = new Client({intents: ["GUILDS", "GUILD_MESSAGES", "DIRECT_MESSAGES", "GUILD_VOICE_STATES"], partials: ["CHANNEL"] });
+const client = new Client({intents: ["GUILDS", "GUILD_MESSAGES", "GUILD_MESSAGE_REACTIONS", "DIRECT_MESSAGES", "GUILD_VOICE_STATES"], partials: ["CHANNEL"] });
 
 ["commands"].forEach(x => client[x] = new Collection());
 ["command", "event"].forEach(x => require(`./handlers/${x}.js`)(client));
 
 client.login(process.env.token);
+
+client.on('messageReactionAdd', (reaction, user) => {
+    console.log("first check");
+  });
