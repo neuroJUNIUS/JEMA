3c21be9b89ae107bfdfb2dfebb801ac35e436a0c
Betradium <matasraila123@gmail.com>
2022-05-09 21:41:59 +0300

    spamChange

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -2,57 +2,64 @@ const anti_spam = require('./anti-spam.module/spam-protection.js');
 const anti_swears = require('./bad-word.module/bad-word.js');
 const Discord  = require('discord.js');
 
-module.exports = async(client, message) =>
-{
+module.exports = async(client, message) => {
     const [cmd, ...args] = message.content
     .trim()
     .substring(5)
     .split(/\s+/);
 
     let cmdfile = client.commands.get(cmd);
-     if(message.author.bot) return;
+    if(message.author.bot) return;
+
+    try {
+        xp_system.addXp(client,message,1, (error) => {
+            if(error) {
+                console.log(`❌There was an error with xp system!`);
+            }
+        });
+        xp_system.addLevel(client,message, (error) => {
+            if(error) {
+                console.log(`❌There was an error with xp system!`);
+            }
+
+        });
+    } catch(e) {
+        console.log(`❌There was an error with xp system!`);
+    }
 
      if(!message.content.startsWith("jema"))
      {
+        try {
+            anti_swears.check(client, message, (error) => {
+                if(error) {
+                    console.log(`❌ There was an error with anti-swear word module...`);
+                }
+            });            
+        } catch {
+            console.log(`❌ There was an error with anti-swear word module...`);
+        }
 
-        anti_swears.check(client, message, (error) => {
-            if(error) {
-                console.log(`❌ There was an error with anti-swear word module...`);
-
-        const illegal_words = ["pavyzdys", "blogaszodis"];
-        let channel = message.channel; 
-        channel.messages.fetch({limit: 5 }).then(messages => {
-        let notsame = 1;
-        let arr = [];
-        for(let [key, value] of messages)
-            {
-                arr.push(value);
-            }
-            let text = message.content;
-            const splitMessage = text.split(/[ &()_+-={};':",.<>?@\r?\n]/);
-        for (let i = 0; i < illegal_words.length; i++)
-            {
-            if(splitMessage.includes(illegal_words[i]))
-            {
-                message.channel.bulkDelete(1);
-            }
-        }   
-        
-        for(let i = 1; i < 5; i++)
-        {
-            if(arr[i-1].content != arr[i].content || arr[i-1].author != arr[i].author)
-            {
-                notsame = 0;
-
-            }
-        });
-        anti_spam.check(client, message, (error) => {
+        try {
+            anti_spam.check(client, message, (error) => {
                 if(error) {
                     console.log(`❌ There was an error with anti-spam module...`);
                 }
             });
+        } catch {
+            console.log(`❌ There was an error with anti-spam module...`);         
+        }
+
      } else {
-        console.log(client.malifunctioned);
+        try {
+            xp_system.register(client, message, (error) => {
+                if(error) {
+                    console.log(`❌There was an error with xp system!`);
+                }
+            });
+        } catch(e) {
+            console.log(`❌There was an error with xp system!`);
+        }
+
         if(client.malifunctioned.get(cmd)) {
             message.channel.send({
                 content:`:pray: Command ${cmd} malifunctioned... Our team is working on a fix...`,
@@ -61,23 +68,13 @@ module.exports = async(client, message) =>
                 ]
             });
         }
-
-        
-        })
-        return;
-     }
-     else
-     {
-	 if(cmdfile && args == "help")
-		{
+	    if(cmdfile && args == "help") {
 			message.reply(cmdfile.help.name + ": " + cmdfile.help.description);
 		}
-		else if(!cmdfile && args == "help")
-		{
+		else if(!cmdfile && args == "help") {
 			message.reply("entered command does not exist");
 		}
-         else if(cmdfile)
-         {
+        else if(cmdfile) {
              cmdfile.run(client, message, args, (error) => {
                 if(error) {
                     console.log(`❌ ${cmdfile.help.name} doesn't work! Turning this command off...`);
@@ -88,10 +85,8 @@ module.exports = async(client, message) =>
                 }
             });       
          }
-		 else
-		 {
-			 message.reply("entered command does not exist");
-		 }
+		 else {
+			message.reply("entered command does not exist");
+		}
      }
  }
-
