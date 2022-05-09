bd9674ce303fe025fb2caedd73cd94b4f613b67b
itastylt <100284073+itastylt@users.noreply.github.com>
2022-05-09 21:53:46 +0300

    Create spamChange.js

---
+++ src/commands/moderation/spamChange.js
@@ -0,0 +1,27 @@
+module.exports = {
+    run:
+        async (client,message,args, error) => {
+            try {
+                if (message.member.permissions.has("ADMINISTRATOR"))
+                {
+                    let text = message.content;
+					const array = text.split(" ");
+                    if(array[2]<2) {
+                        return message.reply("The amount of messages for it to be considered spam can't be less than 2");
+                    }
+                    else {
+                        spamAmount=array[2];
+                        return message.reply(`The amount of same messages allowed: ${spamAmount}`);
+                    }
+                }
+                else
+                    return message("No perms + L");
+            } catch(e) {
+                return error(e);
+            }
+        },
+    help: {
+        name:"spamChange",
+        description:"Changes the amount of same messages for it to be considered spam"
+    }
+}
