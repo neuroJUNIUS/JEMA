20062cdb563dead4adbe9e6483bd3dee443f5c10
Betradium <matasraila123@gmail.com>
2022-05-18 21:34:03 +0300

    Update bad-word.js

--- src/events/bad-word.module/bad-word.js
+++ src/events/bad-word.module/bad-word.js
@@ -7,14 +7,16 @@ module.exports = {
     check:
         async (client, message, error) => {
             try {
-                const illegal_words = ["pavyzdys", "blogaszodis"];
-                let text = message.content;
-                const splitMessage = text.split(/[ &()_+-={};':",.<>?@\r?\n]/);
+                const illegal_words = ["fuck", "pavyzdys", "blogaszodis", "whore", "shit", "arse", "ass", "bitch", "bastard", "bollocks", "bullshit", "cock", "crap", "cunt", "nigga", "nigger", "piss", "prick", "slut", "twat", "faggot"];
+                let messageText = message.content;
+                let text=messageText.toLowerCase();
 
                 for (let i = 0; i < illegal_words.length; i++) {
 
-                    if(splitMessage.includes(illegal_words[i])) {
+                    if(text.indexOf(illegal_words[i]) > -1) {
                         message.channel.bulkDelete(1);
+                        text="good";
+                        break;
                     }
                 }  
             } catch(e) {
